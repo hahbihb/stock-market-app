78bb73df7c42c06daf5f9509f74733e25545d6f7
hahbihb <hsalami612@gmail.com>
2025-01-16 02:31:48 +0100

    adjusting hidden

---
+++ src/hidden.js
@@ -0,0 +1,10 @@
+export const RAPID_PROFILE_KEY =
+  "5cfab8ac59msh655ecd33d6f3202p14af5ejsn5383a3b2b232";
+export const RAPID_PRICE1_KEY =
+  "5cfab8ac59msh655ecd33d6f3202p14af5ejsn5383a3b2b232";
+export const RAPID_PRICE2_KEY =
+  "baef58f377msh40512b46d6da434p19f373jsn66b317d49485";
+export const RAPID_NEWS1_KEY =
+  "178d444d25msh3a17fb33f97b4dfp190960jsn9cda7403cf9a";
+export const RAPID_NEWS2_KEY =
+  "5cfab8ac59msh655ecd33d6f3202p14af5ejsn5383a3b2b232";
